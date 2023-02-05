100d4098fce856e18e70ab805c776eff07877786
singhtushar689 <singhtushar689@gmail.com>
2023-02-05 15:36:21 +0530

    [Tushar] add: Initial Position

---
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class SnakeAndLadder
+    {
+        public void Game()
+        {
+            int initialPos = 0;
+            Console.WriteLine("Player is at " + initialPos);
+        }
+    }
+}
