724d77caa2ea209e976788642cd3171999191795
singhtushar689 <singhtushar689@gmail.com>
2023-02-06 23:15:25 +0530

    [Tushar] add: Player Check a Option

--- SnakeAndLadderProblem/SnakeAndLadder.cs
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -10,12 +10,34 @@ namespace SnakeAndLadderProblem
     {
         public void Game()
         {
-            int initialPos = 0;
-            Console.WriteLine("Player is at " + initialPos);
+            const int snake = 2;const int ladder = 1;
+            int position = 0;
 
             Random random = new Random();
             int rollDice = random.Next(1, 7);
-            Console.WriteLine("The Roll dice is " + rollDice);
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case ladder :
+                    Console.WriteLine("Ladder");
+                    position += rollDice;
+                    break;
+                case snake :
+                    Console.WriteLine("Snake");
+                    if(position - rollDice < 0)
+                    {
+                        position = 0;
+                    }
+                    else
+                    {
+                        position -= rollDice;
+                    }
+                    break;
+                default:
+                    break;
+            }          
+            Console.WriteLine("Value of Dice is " + rollDice);           
+            Console.WriteLine("Player is at " + position);
         }
     }
 }
