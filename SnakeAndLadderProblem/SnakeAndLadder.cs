8556322599fa3bd99627d1b764fa2d85c499fd03
singhtushar689 <singhtushar689@gmail.com>
2023-02-06 23:31:37 +0530

    [Tushar] add: Winning at position 100

--- SnakeAndLadderProblem/SnakeAndLadder.cs
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -10,34 +10,37 @@ namespace SnakeAndLadderProblem
     {
         public void Game()
         {
-            const int snake = 2;const int ladder = 1;
+            const int snake = 2; const int ladder = 1; const int win = 100;
             int position = 0;
-
-            Random random = new Random();
-            int rollDice = random.Next(1, 7);
-            int option = random.Next(0, 3);
-            switch (option)
+            while (position < win)
             {
-                case ladder :
-                    Console.WriteLine("Ladder");
-                    position += rollDice;
-                    break;
-                case snake :
-                    Console.WriteLine("Snake");
-                    if(position - rollDice < 0)
-                    {
-                        position = 0;
-                    }
-                    else
-                    {
-                        position -= rollDice;
-                    }
-                    break;
-                default:
-                    break;
-            }          
-            Console.WriteLine("Value of Dice is " + rollDice);           
-            Console.WriteLine("Player is at " + position);
+                Random random = new Random();
+                int rollDice = random.Next(1, 7);
+                int option = random.Next(0, 3);
+
+                switch (option)
+                {
+                    case ladder:
+                        Console.WriteLine("Ladder");
+                        position += rollDice;
+                        break;
+                    case snake:
+                        Console.WriteLine("Snake");
+                        if (position - rollDice < 0)
+                        {
+                            position = 0;
+                        }
+                        else
+                        {
+                            position -= rollDice;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("Value of Dice is " + rollDice);
+                Console.WriteLine("Player is at " + position);
+            }
         }
     }
 }
